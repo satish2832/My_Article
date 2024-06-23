1e92f0dc1dd9027bde7c702585a7bd84bf4a4d97
satish chintapalli <satish_chintapalli@epam.com>
2024-06-23 13:46:36 +0530

    Updated

--- TechPortalWeb/Content/Site.js
+++ TechPortalWeb/Content/Site.js
@@ -1,6 +1,7 @@
-﻿$("#btnBookDemo").bind('click', function () {
-    $("#divBookDemo").modal({
+﻿document.getElementById('btnBookDemo').addEventListener('click', function () {
+    var divBookDemo = new bootstrap.Modal(document.getElementById('divBookDemo'), {
         backdrop: 'static',
         keyboard: false
-    })
-}); 
+    });
+    divBookDemo.show();
+});
